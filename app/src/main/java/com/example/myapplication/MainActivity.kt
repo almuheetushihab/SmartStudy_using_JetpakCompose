55f9512287ab302336a2b5794db2eee0f3a1c6ba
almuheetushihab <almuheetu@gmail.com>
2024-11-25 17:55:58 +0600

    changing some text in this app tittle

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android For using",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
