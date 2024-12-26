bb58787641b865651ef6724240271748be50acd5
almuheetushihab <almuheetu@gmail.com>
2024-12-26 23:05:22 +0600

    text change

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello this is a testing for program $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
